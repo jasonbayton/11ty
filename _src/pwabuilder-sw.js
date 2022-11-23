177736e30b64344be1f5401e0e4dca149662971a
Jason Bayton <jason@bayton.org>
2022-11-23 15:17:05 +0000

    adjust PWA requirements

--- _src/pwabuilder-sw.js
+++ _src/pwabuilder-sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-const CACHE = "offline.html";
+const CACHE = "offline-pages";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
