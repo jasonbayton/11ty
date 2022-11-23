15e12e22c801cac171758e79d36c8d2d8bb5fee6
Jason Bayton <jason@bayton.org>
2022-11-23 15:23:08 +0000

    revert

--- _src/pwabuilder-sw.js
+++ _src/pwabuilder-sw.js
@@ -29,9 +29,7 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => {
-        cache.add(new Request(offlineFallbackPage));
-      })
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
