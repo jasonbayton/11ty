b7ddace1a6c720f959677818efd90616343eecf9
Jason Bayton <jason@bayton.org>
2022-11-23 14:10:46 +0000

    adjust this again

--- _src/pwabuilder-sw.js
+++ _src/pwabuilder-sw.js
@@ -5,8 +5,8 @@ const CACHE = "pwabuilder-offline-page";
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = [
-  '/offline.html',
+const offlineFallbackPage = "/offline.html";
+const offlinecache = [
   '/',
   '/android/',
   '/css/core.css',
@@ -31,7 +31,7 @@ self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
       .then((cache) => {
-        return cache.addAll(offlineFallbackPage)
+        cache.add(offlineFallbackPage)
       })
   );
 });
