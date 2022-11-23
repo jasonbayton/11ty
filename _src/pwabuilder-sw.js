a20b15ad387376ceebd9e29b074d772d2cc69a52
Jason Bayton <jason@bayton.org>
2022-11-23 14:54:17 +0000

    update workbox

--- _src/pwabuilder-sw.js
+++ _src/pwabuilder-sw.js
@@ -2,7 +2,7 @@
 
 const CACHE = "pwabuilder-offline-page";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "offline.html";
