9c2518bcd86a053df0aebf4a0abe40266a1037d6
Jason Bayton <jason@bayton.org>
2023-04-19 00:00:32 +0100

    Update pwabuilder-sw.js

--- _src/pwabuilder-sw.js
+++ _src/pwabuilder-sw.js
@@ -1,3 +1,5 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
+
 import { precacheAndRoute } from 'workbox-precaching';
 import { CacheFirst } from 'workbox-strategies';
 
