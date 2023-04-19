d40759b3a9d64a0a2a91f2b0b2ff804d644c0535
Jason Bayton <jason@bayton.org>
2023-04-19 14:30:44 +0100

    big phat pwa

--- _src/pwabuilder-sw.js
+++ _src/pwabuilder-sw.js
@@ -1,9 +1,15 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
+// Define the name of the cache
 const CACHE = "offline-pages";
-const offlineFallbackPage = "/offline.html";
+
+// Define the name of the background sync queue
 const bgSyncQueueName = "bg-sync-queue";
 
+// Define the offline fallback page
+const offlineFallbackPage = "/offline.html";
+
+// Define an array of URLs to be cached offline
 const offlinecache = [
   '/',
   '/android/',
@@ -15,23 +21,30 @@ const offlinecache = [
   offlineFallbackPage
 ];
 
+// Event listener for the message event
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
 
+// Event listener for the install event
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     Promise.all([
+      // Add the offline cache to the cache storage
       caches.open(CACHE)
         .then((cache) => cache.addAll(offlinecache)),
+
+      // Register the background sync with the name and options
       self.registration.registerBackgroundSync({
         name: bgSyncQueueName,
         options: {
           maxRetentionTime: 60 * 24 // Retry for up to 24 hours
         }
       }),
+
+      // Register the periodic sync with the tag and options
       self.registration.periodicSync.register('sync-data', {
         minInterval: 60 * 60 * 24 // Sync every 24 hours
       })
@@ -39,17 +52,31 @@ self.addEventListener('install', async (event) => {
   );
 });
 
+// Check if navigation preload is supported
 if (workbox.navigationPreload.isSupported()) {
+  // Enable navigation preload
   workbox.navigationPreload.enable();
 }
 
+// Register a route to cache all requests
 workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
+    cacheName: CACHE,
+    plugins: [
+      // Print a message to the console when a response is fetched from the network
+      new workbox.expiration.Plugin({
+        onCacheEntryAdded: () => console.log('New entry added to cache.'),
+        onCacheEntryUpdated: () => console.log('Entry updated in cache.'),
+        onCacheEntryDeleted: () => console.log('Entry deleted from cache.'),
+        onQuotaExceeded: () => console.warn('Quota exceeded.'),
+        onExpirationComplete: () => console.log('Expired entries removed from cache.')
+      })
+    ]
   })
 );
 
+// Event listener for the fetch event
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
     event.respondWith((async () => {
@@ -72,22 +99,100 @@ self.addEventListener('fetch', (event) => {
   }
 });
 
+// Event listener for the sync event
 self.addEventListener('sync', (event) => {
   if (event.tag === bgSyncQueueName) {
     event.waitUntil(handleBackgroundSync());
   }
 });
 
+// Event listener for the periodic sync event
 self.addEventListener('periodicsync', (event) => {
   if (event.tag === 'sync-data') {
-    event.waitUntil(handlePeriodicSync());
+  event.waitUntil(handlePeriodicSync());
   }
-});
+  });
 
+// Handle background sync requests here
 async function handleBackgroundSync() {
-  // Handle background sync requests here
+  try {
+    // Get the background sync queue.
+    const bgSyncQueue = await getBackgroundSyncQueue(bgSyncQueueName);
+
+    // Iterate over the queue and handle each request.
+    for (const syncRequest of bgSyncQueue) {
+      const { id, request } = syncRequest;
+
+      // Attempt to fetch the request from the network.
+      const networkResponse = await fetch(request);
+
+      // If the request was successful, update the cache with the new data.
+      if (networkResponse.ok) {
+        await caches.open(CACHE)
+          .then((cache) => cache.put(request, networkResponse.clone()));
+
+        // Remove the request from the queue.
+        await removeRequestFromQueue(bgSyncQueueName, id);
+      }
+    }
+  } catch (error) {
+    console.error('Error handling background sync:', error);
+  }
 }
 
+// Handle periodic sync requests here
 async function handlePeriodicSync() {
-  // Handle periodic sync requests here
+  try {
+    console.log("Periodic Sync Started");
+
+    // Cache static assets for offline use
+    const cacheName = "static-assets";
+    const urlsToCache = [
+      "/offline.html",
+      "/css/base.css",
+      "/js/darkmode.js",
+      "/js/darkmodetoggle.js",
+      "/img/bayton_logos/bayton_rectangle_light.svg",
+      "/img/bayton_logos/bayton_rectangle.svg",
+    ];
+    const cache = await caches.open(cacheName);
+    await cache.addAll(urlsToCache);
+
+    // Update cache with new content
+    const updateCache = async () => {
+      const response = await fetch("/api/data");
+      const cache = await caches.open("data-cache");
+      cache.put("/api/data", response.clone());
+    };
+    await updateCache();
+
+    // Provide fallback page when there is no network connectivity
+    const fallbackPage = async () => {
+      const cache = await caches.open("offline-cache");
+      const cachedResponse = await cache.match("/offline.html");
+      return cachedResponse || Response.error();
+    };
+
+    // Invalidate outdated cache entries
+    const invalidateCache = async () => {
+      const cache = await caches.open("data-cache");
+      const keys = await cache.keys();
+      const now = Date.now();
+      keys.forEach(async (key) => {
+        const response = await cache.match(key);
+        const headers = response.headers;
+        const dateHeader = headers.get("date");
+        const date = new Date(dateHeader).getTime();
+        if (now - date > 24 * 60 * 60 * 1000) {
+          await cache.delete(key);
+        }
+      });
+    };
+    await invalidateCache();
+
+    console.log("Periodic Sync Successful");
+  } catch (err) {
+    console.error("Periodic Sync Failed: ", err);
+  }
 }
+
