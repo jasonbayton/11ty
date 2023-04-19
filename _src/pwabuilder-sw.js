ec23236a5c3ffa9ac95ab4b3bf36eaa1e706e3a3
Jason Bayton <jason@bayton.org>
2023-04-19 13:40:49 +0100

    update PWA

--- _src/pwabuilder-sw.js
+++ _src/pwabuilder-sw.js
@@ -1,12 +1,10 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
-import { precacheAndRoute } from 'workbox-precaching';
-import { CacheFirst } from 'workbox-strategies';
+const CACHE = "offline-pages";
+const offlineFallbackPage = "offline.html";
+const bgSyncQueueName = "bg-sync-queue";
 
-const CACHE_NAME = 'my-cache-v1';
-
-// Precache a list of URLs
-precacheAndRoute([
+const offlinecache = [
   '/',
   '/android/',
   '/css/base.css',
@@ -14,20 +12,82 @@ precacheAndRoute([
   '/js/darkmodetoggle.js',
   '/img/bayton_logos/bayton_rectangle_light.svg',
   '/img/bayton_logos/bayton_rectangle.svg',
-  '/offline.html'
-]);
+  offlineFallbackPage
+];
 
-// Use the CacheFirst strategy to serve precached URLs
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    Promise.all([
+      caches.open(CACHE)
+        .then((cache) => cache.addAll(offlinecache)),
+      self.registration.registerBackgroundSync({
+        name: bgSyncQueueName,
+        options: {
+          maxRetentionTime: 60 * 24 // Retry for up to 24 hours
+        }
+      }),
+      self.registration.periodicSync.register('sync-data', {
+        minInterval: 60 * 60 * 24 // Sync every 24 hours
+      })
+    ])
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
 workbox.routing.registerRoute(
-  ({url}) => {
-    return precacheUrls.indexOf(url.pathname) !== -1;
-  },
-  new CacheFirst({
-    cacheName: CACHE_NAME
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
   })
 );
 
-// Serve offline fallback page for all navigation requests
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL('/offline.html')
-);
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
+
+self.addEventListener('sync', (event) => {
+  if (event.tag === bgSyncQueueName) {
+    event.waitUntil(handleBackgroundSync());
+  }
+});
+
+self.addEventListener('periodicsync', (event) => {
+  if (event.tag === 'sync-data') {
+    event.waitUntil(handlePeriodicSync());
+  }
+});
+
+async function handleBackgroundSync() {
+  // Handle background sync requests here
+}
+
+async function handlePeriodicSync() {
+  // Handle periodic sync requests here
+}
