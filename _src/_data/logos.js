4708a6b36172749682d717611768ac2cdad22f82
Jason Bayton <jason@bayton.org>
2023-05-20 21:58:16 +0100

    reimplement old logo area with random logos

--- _src/_data/logos.js
+++ _src/_data/logos.js
@@ -4,7 +4,23 @@ const path = require('path');
 // Adjust the path to match your project structure
 const logoDirectory = path.resolve(__dirname, '../_includes/_assets/img/logos');
 
-const logos = fs.readdirSync(logoDirectory).map(file => `/img/logos/${file}`);
+let logos = fs.readdirSync(logoDirectory).map(file => `/img/logos/${file}`);
 
-module.exports = logos;
+// Function to generate a random integer between min and max (inclusive)
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Randomly select 8 logos
+let selectedLogos = [];
+for (let i = 0; i < 16; i++) {
+    let index = getRandomInt(0, logos.length - 1);
+    selectedLogos.push(logos[index]);
+    logos.splice(index, 1);  // Remove the selected logo from the array to avoid duplicates
+}
+
+module.exports = selectedLogos;
+
 
