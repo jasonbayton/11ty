939d62f0affd168b13cb6d29c8418f4d85885514
Jason Bayton <jason@bayton.org>
2023-06-03 07:03:00 +0100

    Update darkmode.js

--- _src/_includes/_assets/js/darkmode.js
+++ _src/_includes/_assets/js/darkmode.js
@@ -1,3 +1,15 @@
-var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-if (storedTheme)
-  document.documentElement.setAttribute('data-theme', storedTheme);
+try {
+    var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+
+    if (storedTheme) {
+        // Validate the theme value
+        if (['dark', 'light'].includes(storedTheme)) {
+            document.documentElement.setAttribute('data-theme', storedTheme);
+        } else {
+            console.warn(`Unexpected theme value: ${storedTheme}`);
+        }
+    }
+} catch (error) {
+    // Handle any errors accessing localStorage or matchMedia
+    console.error('Error applying theme', error);
+}
